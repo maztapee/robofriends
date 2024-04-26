8f7dc4c96e7953bef2e22107b3368f0daae45c8d
maztapee <91094826+maztapee@users.noreply.github.com>
2024-04-26 17:16:21 +0100

    Improved the responsiveness par Abbaxxee

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,38 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavigationBar.css'
 
 const NavigationBar = ({onNavigate}) =>{
+
+    const [isExpanded, setIsExpanded] = useState(false);
+    const togglePanel = (e) => {
+        e.stopPropagation();  // prevent panel from closing when clicking inside the
+        e.preventDefault();
+        console.log(isExpanded);
+        setIsExpanded(!isExpanded);
+    };
 
     const handleNavigation = (criteria)=>{
         onNavigate(criteria);  //passing the criteria to App.js for navigation function
     };
 
     return (
-        <div  className='flex flex-wrap: wrap'>
-            <nav>
-                <ul className="flex justify-between">
-                    <li>
-                        <button className='bg-lightest-blue grow bw1 shadow-5' onClick={()=> handleNavigation('friends')}>
-                            All Robofriends
-                        </button>
-                    </li>
-                    <li>
-                        <button className='bg-lightest-blue grow bw1 shadow-5' onClick={()=> handleNavigation('add')}>
-                            Add a Robofriend
-                        </button>
-                    </li>
-                    <li>
-                        <button className='bg-lightest-blue grow bw1 shadow-5' onClick={()=> handleNavigation('new')}>
-                            Newest Robofriends
-                        </button>
-                    </li>
-                    <li>
-                        <button className='bg-lightest-blue grow bw1 shadow-5' onClick={()=> handleNavigation('old')}>
-                            Oldest Robofriends
-                        </button>
-                    </li>
-                </ul>
-            </nav>
+        <div  className='nav-bar'>
+            <div
+                className={`collapsible-panel ${isExpanded ? 'expanded':'collapsed'}`}
+                onMouseEnter={togglePanel}
+                onMouseLeave={(e)=>{
+
+                   togglePanel(e)
+                }}
+            >   
+                { isExpanded? (
+                    <ul className="navbar-btn">
+                        <li>
+                            <button className='bg-lightest-blue grow bw1 shadow-5' onClick={()=> handleNavigation('friends')}>
+                                All Robofriends
+                            </button>
+                        </li>
+                        <li>
+                            <button className='bg-lightest-blue grow bw1 shadow-5' onClick={()=> handleNavigation('add')}>
+                                Add a Robofriend
+                            </button>
+                        </li>
+                        <li>
+                            <button className='bg-lightest-blue grow bw1 shadow-5' onClick={()=> handleNavigation('new')}>
+                                Newest Robofriends
+                            </button>
+                        </li>
+                        <li>
+                            <button className='bg-lightest-blue grow bw1 shadow-5' onClick={()=> handleNavigation('old')}>
+                                Oldest Robofriends
+                            </button>
+                        </li>
+                    </ul>
+                    ):(
+                        <div className="collapsed-content">
+                        <div className="tooltip">
+                            <hr/>
+                            <hr/>
+                        </div>
+                    </div>
+                    )}
+  
+                </div>
             
         </div>
             
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchBox = ({search_field, searchChange}) =>{
     return (
-        <div className='pa2 tc pt3'>
+        <div className='search-box pa2 tc pt3'>
             <input className='pa3 ba b--green bg-lightest-blue br3 mt2'
                 type='search' 
                 placeholder='Search Robots'
